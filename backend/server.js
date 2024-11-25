e4946e1355ba87d320fc9faedc2e7e5f791b5333
pratikmtele <pratiktele4@gmail.com>
2024-11-25 17:28:44 +0530

    port updated

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,45 @@
-import express  from "express"
-import cors from 'cors'
-import { connectDB } from "./config/db.js"
-import userRouter from "./routes/userRoute.js"
-import foodRouter from "./routes/foodRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
-import categoryRouter from "./routes/categoryRoute.js"
-import discountRouter from "./routes/discountRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import foodRouter from "./routes/foodRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import categoryRouter from "./routes/categoryRoute.js";
+import discountRouter from "./routes/discountRoute.js";
 
 // app config
-const app = express()
-const port = 4000
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 // set ejs as view engine
-app.set('view engine', 'ejs')
-
+app.set("view engine", "ejs");
 
 // middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // db connection
-connectDB()
+connectDB();
 
 // api endpoints
-app.use("/api/user", userRouter)
-app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/image", express.static('uploads/categories'))
-app.use("/api/cart", cartRouter)
-app.use("/api/order",orderRouter)
-app.use("/api/category", categoryRouter)
-app.use("/api/discount", discountRouter)
+app.use("/api/user", userRouter);
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/image", express.static("uploads/categories"));
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/discount", discountRouter);
 
 app.get("/", async (req, res) => {
-    res.send("API Working")
+  res.send("API Working");
 });
 
-app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
+app.listen(process.env.PORT, () =>
+  console.log(`Server started on http://localhost:${process.env.PORT}`)
+);
 
 export default app;
